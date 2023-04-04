1a4fe11bc3c6b8b654f8ff644365dca93ad27ccd
Evgeny Bessonnitsyn <81041213+Turukmokto@users.noreply.github.com>
2023-04-04 23:31:34 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1112700607 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return np.mean(x * 2 / 53 ** 2) # Ваш ответ
